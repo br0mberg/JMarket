541c9d7d4b230ebaa36e1564f3b28452ccfc48f1
Brombin Andrey <arcadka@gmail.com>
2024-08-23 00:34:35 +0300

    refactoring

--- src/main/java/ru/brombin/JMarket/config/SecurityConfig.java
+++ src/main/java/ru/brombin/JMarket/config/SecurityConfig.java
@@ -1,26 +1,15 @@
 package ru.brombin.JMarket.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.stereotype.Component;
 import ru.brombin.JMarket.security.AuthProviderImpl;
+import ru.brombin.JMarket.security.PersonDetails;
 
-@EnableWebSecurity
-@Component
-public class SecurityConfig extends WebSecurityConfiguration {
+@Configuration
+public class SecurityConfig {
 
-    private final AuthProviderImpl authProvider;
-
-    @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
-    }
-
-
-    // Настраиваем аутентификацию
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider);
-    }
 }
