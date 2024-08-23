c47e9ad2c936ea48cadfff6a4da40474a831534f
Brombin Andrey <arcadka@gmail.com>
2024-08-23 23:07:59 +0300

    Add basic security cinfiguration (boot3.0) and custom login page

--- src/main/java/ru/brombin/JMarket/config/SecurityConfig.java
+++ src/main/java/ru/brombin/JMarket/config/SecurityConfig.java
@@ -1,15 +1,72 @@
 package ru.brombin.JMarket.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import ru.brombin.JMarket.security.AuthProviderImpl;
-import ru.brombin.JMarket.security.PersonDetails;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import ru.brombin.JMarket.services.PersonDetailsService;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
+    private final PersonDetailsService personDetailsService;
+
+    @Autowired
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
+    }
+
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        /*http.csrf(csrf -> csrf.disable())
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()
+                        .anyRequest().authenticated());*/
+
+        http.authorizeHttpRequests(auth -> auth.requestMatchers("/auth/login").permitAll())
+                .formLogin(form -> form.
+                        loginPage("/auth/login")
+                        .loginProcessingUrl("/process_login")
+                        .defaultSuccessUrl("/items", true)
+                        .failureUrl("/auth/login?error"))
+                .authenticationProvider(authenticationProvider());
+
+        // отдаем страницу, и указываем куда данные с формы отправлять (нам самим их обрабатывать не надо)
+        // поля в форме обязательно username и password иначе spring security не увидит
+
+        return http.build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+
+        authenticationProvider.setUserDetailsService(personDetailsService);
+        authenticationProvider.setPasswordEncoder(getPasswordEncoder());
+
+        return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
---
+++ src/main/java/ru/brombin/JMarket/controller/AuthController.java
@@ -0,0 +1,15 @@
+package ru.brombin.JMarket.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")
+public class AuthController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "auth/login";
+    }
+}
